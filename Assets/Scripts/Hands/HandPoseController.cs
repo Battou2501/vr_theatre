350774fa5ef57371dc5e7c6c3c903f8ad86c0118
Battou2501 <a.egorov.ccr@gmail.com>
2025-02-08 00:32:58 +0300

    *assets structure cleanup

--- Assets/Scripts/Hands/HandPoseController.cs
+++ Assets/Scripts/Hands/HandPoseController.cs
@@ -3,22 +3,37 @@ using UnityEngine.InputSystem;
 
 public class HandPoseController : MonoBehaviour
 {
+    public enum Handedness
+    {
+        Left = 0,
+        Right = 1
+    }
     [SerializeField]
     ThumbPoseController thumbPoseController;
     [SerializeField]
     IndexPoseController indexPoseController;
     [SerializeField]
     LowerFingersPoseController lowerFingersPoseController;
+    [SerializeField]
+    Handedness hand;
+
+    bool is_initialized;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void init()
     {
+        thumbPoseController.init(hand == Handedness.Left);
+        indexPoseController.init(hand == Handedness.Left);
+        lowerFingersPoseController.init(hand == Handedness.Left);
         
+        is_initialized = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(!is_initialized) return;
         
+        thumbPoseController.update_current_pose();
+        indexPoseController.update_current_pose();
+        lowerFingersPoseController.update_current_pose();
     }
 }
